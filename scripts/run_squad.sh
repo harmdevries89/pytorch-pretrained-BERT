7f3bd4f8224c3f49bafbe1b2e18915063674106e
Harm de Vries <harm.de-vries@elementai.com>
2019-07-12 09:44:35 -0400

    update docker

--- scripts/run_squad.sh
+++ scripts/run_squad.sh
@@ -12,10 +12,12 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
   examples/run_squad.py \
   --do_train \
   --do_predict \
-  --bert_model bert-large-cased \
+  --bert_model bert-large-uncased \
   --train_file /home/hdvries/data/train-v1.1.json \
   --predict_file /home/hdvries/data/dev-v1.1.json \
-  --train_batch_size 4 \
-  --learning_rate 5e-5 \
-  --num_train_epochs 3 \
+  --train_batch_size 24 \
+  --gradient_accumulation_steps 6 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2 \
+  --do_lower_case \
   --output_dir $EXPERIMENT_DIR \
